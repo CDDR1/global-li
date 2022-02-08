589af87a6a1d1bc0fd63b628757d8a935341fd7a
Carlos Duque <carlos.d.duque.r@gmail.com>
2022-02-07 21:47:13 -0500

    Added transition functionality to navbar

--- js/navbar.js
+++ js/navbar.js
@@ -1,23 +1,12 @@
-const section = document.querySelector('main');
 const header = document.querySelector('header');
 const logo = document.querySelector('.header-logo');
 
-const options = {
-    threshold: 0,
-    rootMargin: '100px 0px 0px 0px',
-};
-
-const observer = new IntersectionObserver((entries, observer) => {
-    entries.forEach(entry => { 
-        if (entry.isIntersecting) {
-            header.classList.add('change-nav');
-            logo.src = '../img/global-li-logo-2.png';
-        }
-        else {    
-            header.classList.remove('change-nav');
-            logo.src = '../img/global-li-logo.png';
-        }
-    });
-}, options);
-
-observer.observe(section);
+window.addEventListener('scroll', () => {
+    header.classList.toggle('change-nav', window.scrollY > 100);
+    if (window.scrollY > 100) {
+        logo.src = '../img/global-li-logo-2.png';
+    }
+    else {
+        logo.src = '../img/global-li-logo.png';
+    }
+});
