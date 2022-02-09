092952f4a628756e8ddb5252d857f9e3e0957b2f
Carlos Duque <carlos.d.duque.r@gmail.com>
2022-02-09 09:41:45 -0500

    Adding Flask to the project

--- static/js/navbar.js
+++ static/js/navbar.js
@@ -4,9 +4,9 @@ const logo = document.querySelector('.header-logo');
 window.addEventListener('scroll', () => {
     header.classList.toggle('change-nav', window.scrollY > 100);
     if (window.scrollY > 100) {
-        logo.src = '../img/global-li-logo-2.png';
+        logo.src = '../static/img/global-li-logo-2.png'; 
     }
     else {
-        logo.src = '../img/global-li-logo.png';
+        logo.src = '../static/img/global-li-logo.png';
     }
 });
