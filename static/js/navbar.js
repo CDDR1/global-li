041fc951f83a2e209c7f52f9a22aa0c95abd1aba
Carlos Duque <carlos.d.duque.r@gmail.com>
2022-02-10 11:37:54 -0500

    Changed navbar style change scroll distance from 100 to 10

--- static/js/navbar.js
+++ static/js/navbar.js
@@ -2,8 +2,8 @@ const header = document.querySelector('header');
 const logo = document.querySelector('.header-logo');
 
 window.addEventListener('scroll', () => {
-    header.classList.toggle('change-nav', window.scrollY > 100);
-    if (window.scrollY > 100) {
+    header.classList.toggle('change-nav', window.scrollY > 10);
+    if (window.scrollY > 10) {
         logo.src = '../static/img/global-li-logo-2.png'; 
     }
     else {
