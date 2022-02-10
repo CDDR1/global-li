c1a8583c3d9b140c9caeb9d6b5ce5facdf450cf4
Carlos Duque <carlos.d.duque.r@gmail.com>
2022-02-10 11:19:46 -0500

    Finished Contact page

--- index.py
+++ index.py
@@ -8,6 +8,18 @@ def index():
 
 @app.route('/about')
 def about():
-    return render_template('about.html')
+    return render_template('about.html', title='About Us')
+
+@app.route('/foreign-direct-investments')
+def investments():
+    return render_template('')
+
+@app.route('/exports')
+def exports():
+    return render_template('')
+
+@app.route('/contact')
+def contact():
+    return render_template('contact.html', title='Contact Us')
 
 # export FLASK_DEBUG=0 to deactivate debug mode
