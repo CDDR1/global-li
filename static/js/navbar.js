e32e78a736c083f3d5d3ce13653ebd61e83e4a0d
Carlos Duque <carlos.d.duque.r@gmail.com>
2022-02-16 07:23:36 -0500

    Saving recent changes

--- static/js/navbar.js
+++ static/js/navbar.js
@@ -1,12 +1,16 @@
 const header = document.querySelector('header');
 const logo = document.querySelector('.header-logo');
+const searchBar = document.querySelector('.search-bar');
 
 window.addEventListener('scroll', () => {
     header.classList.toggle('change-nav', window.scrollY > 10);
+    searchBar.classList.toggle('change-search-bar', window.scrollY > 10);
     if (window.scrollY > 10) {
         logo.src = '../static/img/global-li-logo-2.png'; 
+        document.documentElement.style.setProperty('--search-bar-placeholder-color', 'rgb(161, 161, 161)');
     }
     else {
         logo.src = '../static/img/global-li-logo.png';
+        document.documentElement.style.removeProperty('--search-bar-placeholder-color', 'rgb(161, 161, 161)');
     }
 });
