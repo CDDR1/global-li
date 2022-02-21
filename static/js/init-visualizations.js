e6375aa74695701d0f3c47fefe98621f63777872
Carlos Duque <carlos.d.duque.r@gmail.com>
2022-02-21 07:35:49 -0500

    Saving

--- static/js/init-visualizations.js
+++
@@ -1,7 +0,0 @@
-const initViz = () => {
-    const containerDiv = document.getElementById('vizContainer');
-    url = 'https://public.tableau.com/views/LIFDIDashboard/FDIDashboard';
-    const viz = new tableau.Viz(containerDiv, url);
-};
-
-window.addEventListener('DOMContentLoaded', initViz);
