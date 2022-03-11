57d17e62d19262a84fd0fac40a3663b3401aa2e1
Carlos Duque <carlos.d.duque.r@gmail.com>
2022-03-11 14:54:23 -0500

    Adding mobile navigation bar

--- static/js/collapsable-nav.js
+++ static/js/collapsable-nav.js
@@ -1,10 +1,12 @@
 const hamburgerMenu = document.querySelector('.hamburger-menu');
 const hamburgerMenuLines = document.getElementsByClassName('line');
 const collapsableNav = document.querySelector('.collapsable-nav');
+const mobileNav = document.querySelector('.mobile-nav');
 
 hamburgerMenu.addEventListener('click', () => {
     for (let i = 0; i < hamburgerMenuLines.length; i++) {
         hamburgerMenuLines[i].classList.toggle('active');
     }
     collapsableNav.classList.toggle('active');
+    mobileNav.classList.toggle('height-change');
 });
