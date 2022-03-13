ab96b1819561c3ef966ba6e6b47fba26729dcf22
Carlos Duque <carlos.d.duque.r@gmail.com>
2022-03-12 21:04:45 -0500

    Changed regular for loop for a forEach loop

--- static/js/collapsable-nav.js
+++ static/js/collapsable-nav.js
@@ -4,9 +4,7 @@ const collapsableNav = document.querySelector('.collapsable-nav');
 const mobileNav = document.querySelector('.mobile-nav');
 
 hamburgerMenu.addEventListener('click', () => {
-    for (let i = 0; i < hamburgerMenuLines.length; i++) {
-        hamburgerMenuLines[i].classList.toggle('active');
-    }
+    Array.from(hamburgerMenuLines).forEach(line => line.classList.toggle('active'));
     collapsableNav.classList.toggle('active');
     mobileNav.classList.toggle('height-change');
 });
