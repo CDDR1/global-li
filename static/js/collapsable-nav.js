d238c73f93c5abfea555867340654ec3443d72f0
Carlos Duque <carlos.d.duque.r@gmail.com>
2022-04-07 10:43:33 -0400

    Adding hamburger menu transition effect

--- static/js/collapsable-nav.js
+++ static/js/collapsable-nav.js
@@ -1,10 +1,9 @@
 const hamburgerMenu = document.querySelector('.hamburger-menu');
-const hamburgerMenuLines = document.getElementsByClassName('line');
 const collapsableNav = document.querySelector('.collapsable-nav');
 const mobileNav = document.querySelector('.mobile-nav');
 
 hamburgerMenu.addEventListener('click', () => {
-    Array.from(hamburgerMenuLines).forEach(line => line.classList.toggle('active'));
+    hamburgerMenu.classList.toggle('active');
     collapsableNav.classList.toggle('active');
     mobileNav.classList.toggle('height-change');
-});
+})
