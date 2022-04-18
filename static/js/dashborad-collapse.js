13b6f3f125b3dc6fcaa835c1513da8168e1e1b4b
Carlos Duque <carlos.d.duque.r@gmail.com>
2022-04-18 05:24:54 -0400

    Deleted dashboard-collapse.js file

--- static/js/dashborad-collapse.js
+++
@@ -1,6 +0,0 @@
-const someHeading = document.querySelector('.some-heading');
-const dashboard = document.querySelector('.dashboard-container');
-
-someHeading.addEventListener('click', () => {
-    dashboard.classList.toggle('show-dashboard');
-});
