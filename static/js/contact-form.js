ad7031caf452d9af3230356560e6114e7e3952ae
Carlos Duque <carlos.d.duque.r@gmail.com>
2022-04-27 06:57:16 -0400

    Restoring the collapsable-nav.js file that was accidentally deleted

---
+++ static/js/contact-form.js
@@ -0,0 +1,18 @@
+const form = document.querySelector('#form');
+
+async function handleSubmit(e) {
+    e.preventDefault();
+    const form = new FormData(this);
+    const response = await fetch(this.action, {
+        method: this.method,
+        body: form,
+        headers: {
+            'Accept': 'application/json'
+        }
+    });
+    if (response.ok) {
+        this.reset();
+    }
+}
+
+form.addEventListener('submit', handleSubmit);
