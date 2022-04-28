f7e6b0d2317c8450735e2d09d3f0ad12921ca04d
Carlos Duque <carlos.d.duque.r@gmail.com>
2022-04-27 20:49:19 -0400

    Saving changes done to the fetch API function

--- static/js/init-viz.js
+++ static/js/init-viz.js
@@ -1,3 +1,27 @@
+// TESTING IF THIS IS GOING TO WORK
+const urls = ["https://public.tableau.com/views/Top10LIFDIInvestorsbycountry/Bycountry", 
+"https://public.tableau.com/views/LIFDIEmploymentbycountry/Bycountry", 
+"https://public.tableau.com/views/LIFDIEmployment/LIFDIEmployment"];
+
+const fetchUrls = urls => {
+    urls.forEach((url, index) => {
+        const containerDiv = document.createElement("div");
+        containerDiv.id = `vizContainer${index + 1}`;
+        containerDiv.style.width = "100%";
+        containerDiv.style.height = "100%";
+    
+        const dashboardContainers = document.querySelectorAll('.dashboard-container');
+        dashboardContainers[index].appendChild(containerDiv);
+    
+        const options = {
+            hideTabs: true
+        };
+        const viz = new tableau.Viz(containerDiv, url, options);
+    });
+};
+window.addEventListener('DOMContentLoaded', fetchUrls(urls));
+
+//==================================================================================================
 // const initViz = () => {
 //     const containerDiv1 = document.getElementById("vizContainer1"),
 //         url = "https://public.tableau.com/views/Top10LIFDIInvestorsbycountry/Bycountry",
@@ -28,42 +52,42 @@
 
 // Let's try fetching the API here...
 // FOR REFERENCE: data[0].PageLink[0].LinkName
-const createGridItem = (title, graph, description, index) => {
-    const gridItem = document.createElement('div');
-    gridItem.classList.add('grid-item');
+// const createGridItem = (title, graph, description, index) => {
+//     const gridItem = document.createElement('div');
+//     gridItem.classList.add('grid-item');
 
-    const titleElement = document.createElement('h2');
-    titleElement.classList.add('graph-title');
-    titleElement.textContent = title;
-    gridItem.appendChild(titleElement);
+//     const titleElement = document.createElement('h2');
+//     titleElement.classList.add('graph-title');
+//     titleElement.textContent = title;
+//     gridItem.appendChild(titleElement);
 
-    const graphContainer = document.createElement('div');
-    graphContainer.classList.add('graph-container');
-    graphContainer.innerHTML = graph;
-    gridItem.appendChild(graphContainer);
+//     const graphContainer = document.createElement('div');
+//     graphContainer.classList.add('graph-container');
+//     graphContainer.innerHTML = graph;
+//     gridItem.appendChild(graphContainer);
 
-    const descriptionElement = document.createElement('p');
-    descriptionElement.classList.add('graph-description');
-    descriptionElement.textContent = description;
-    gridItem.appendChild(descriptionElement);
+//     const descriptionElement = document.createElement('p');
+//     descriptionElement.classList.add('graph-description');
+//     descriptionElement.textContent = description;
+//     gridItem.appendChild(descriptionElement);
 
-    if ((index + 1) % 3 === 0) {
-        gridItem.classList.add('grid-item-large');
-    }
+//     if ((index + 1) % 3 === 0) {
+//         gridItem.classList.add('grid-item-large');
+//     }
 
-    const grid = document.querySelector('.dashboard-grid');
-    grid.appendChild(gridItem);
-};
+//     const grid = document.querySelector('.dashboard-grid');
+//     grid.appendChild(gridItem);
+// };
 
-const fetchData = () => {
-    fetch('https://api-li.computertechclub.com/api/Pages/GetallPages')
-        .then(res => res.json())
-        .then(data => {
-            data[0].PageLink.forEach((item, index) => {
-                createGridItem(item.LinkName, item.LinkUrl, item.LinkDescription, index);
-            });
-        })
-        .catch(err => console.log(err));
-};
+// const fetchData = () => {
+//     fetch('https://api-li.computertechclub.com/api/Pages/GetallPages')
+//         .then(res => res.json())
+//         .then(data => {
+//             data[0].PageLink.forEach((item, index) => {
+//                 createGridItem(item.LinkName, item.LinkUrl, item.LinkDescription, index);
+//             });
+//         })
+//         .catch(err => console.log(err));
+// };
 
 // window.addEventListener('DOMContentLoaded', fetchData);
